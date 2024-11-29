5bc5d2bfd8f12a0099a2ca3ff915591f8666a85b
thebabalola <t.babalolajoseph@gmail.com>
2024-11-29 23:22:11 +0100

    logo linked to home page

--- userdash_page/dashbaord.js
+++ userdash_page/dashbaord.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
         settingsPanel.classList.toggle('open');
     });
 
-    // Dark mode toggle
+    // Dark mode togglen
     darkModeToggle.addEventListener('change', function() {
         document.body.classList.toggle('dark-mode', this.checked);
     });
